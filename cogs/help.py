259acd326480e348d97bdd5666c48cdaf24bc74b
joshua <zoniqhedgehog@gmail.com>
2021-11-11 22:42:44 +0100

    Add help cog

---
+++ cogs/help.py
@@ -0,0 +1,12 @@
+from discord.ext import commands
+from utility.helpmenu import PenguinHelp
+
+class HelpCog(commands.Cog, name="HelpMenu"):
+    def __init__(self, bot):
+        self.bot = bot
+        bot.help_command = PenguinHelp()
+        bot.help_command.cog = self
+        # self.help_icon = '<:Discovery:845656527347777548>' # Not relevant as this is for the help menu that this cog allows usage of.
+
+def setup(bot):
+    bot.add_cog(HelpCog(bot))
