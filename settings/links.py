06df89565776a2c2d3975f7d4e25034d52567820
joshua <zoniqhedgehog@gmail.com>
2021-11-12 14:38:23 +0100

    Add joshua, etile, github

--- settings/links.py
+++ settings/links.py
@@ -1,2 +1,7 @@
 support = 'https://discord.gg/QMhvHuGPMx'
 invite = 'https://tryitands.ee/'
+
+joshua = '843866750131109909'
+etile = '856910332290007071'
+
+github = 'https://github.com/BadWolf-Dev/BadWolf-bot'
