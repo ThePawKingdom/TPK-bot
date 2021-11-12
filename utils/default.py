1144d4cd0a0f50e15ea9b1c260a5180e2d78c254
joshua <zoniqhedgehog@gmail.com>
2021-11-12 19:26:59 +0100

    Add find_user

--- utils/default.py
+++ utils/default.py
@@ -3,3 +3,17 @@ def next_level(ctx):
         count = int(2 - ctx.guild.premium_subscription_count)
         txt = f'Next level in **{count}** boosts'
         return txt
+
+async def find_user(ctx, user):
+    user = user or ctx.author
+    if isinstance(user, discord.User):
+        pass
+    elif isinstance(user, str):
+        if not user.isdigit():
+            return None
+        try:
+            user = await ctx.bot.fetch_user(user)
+        except Exception:
+            return None
+
+    return user
