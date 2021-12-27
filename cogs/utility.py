6673e1ad8be9ad41a7875b09e375f06b9dfc20ab
Joshua <ZoniqHedgehog@gmail.com>
2021-12-27 16:40:27 +0100

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -54,11 +54,10 @@ class Utility(commands.Cog, name="Utility"):
         e.description = f"""
 __**General information**__
 Developers:
-- **[{etile}](https://discord.com/users/{links.etile})**
 - **[{joshua}](https://discord.com/users/{links.joshua})**
 
 Library: [Enhanced-discord.py {discord.__version__}](https://github.com/iDevision/enhanced-discord.py)
-Links: [support server]({links.support}) | [Invite me]({links.invite}) | [github]({links.github})
+Links: [support server]({links.support}) | [github]({links.github})
 
 __**System**__
 System Software: `{platform.system()} {platform.release()}`
@@ -229,22 +228,9 @@ __**Statistics**__
         await ctx.send(embed=e)
 
     @commands.command()
-    async def invite(self, ctx):
-        """ Invite me to your server! """
-        e = discord.Embed(description=f"You can invite me to your server [here]({links.invite})" + '\n'
-                                      f"Invite without slash commands [here]({links.plaininvite})", color=colors.green)
-        await ctx.send(embed=e)
-
-    @commands.command()
     async def privacy(self, ctx):
         """ Get the privacy policy for BadWolf """
-        e = discord.Embed(description=f"{self.bot.user.name} does not gather data. For questions, [join the support server]({links.support})", color=colors.green)
-        await ctx.send(embed=e)
-
-    @commands.command()
-    async def support(self, ctx):
-        """ Get the support server link """
-        e = discord.Embed(description=f"You can join our support server [here]({links.support})", color=colors.green)
+        e = discord.Embed(description=f"You can check out our privacy policy [here](https://psychops.tk/legal.php#collected).", color=colors.green)
         await ctx.send(embed=e)
 
     @commands.command()
@@ -268,26 +254,26 @@ __**Statistics**__
         embed.set_image(url=display)
         await ctx.send(embed=embed)
 
-    @commands.command()
-    @commands.cooldown(1, 30, commands.BucketType.user)
-    async def suggest(self, ctx, *, suggestion):
-        """ Make a suggestion for BadWolf """
-        channel = await self.bot.fetch_channel(907617618180075580)
+    # @commands.command()
+    # @commands.cooldown(1, 30, commands.BucketType.user)
+    # async def suggest(self, ctx, *, suggestion):
+    #     """ Make a suggestion for BadWolf """
+    #     channel = await self.bot.fetch_channel(907617618180075580)
+    
+    #     if len(suggestion) > 1000:
+    #         return await ctx.send(":warning: Suggestions can not be more then 1000 characters.")
 
-        if len(suggestion) > 1000:
-            return await ctx.send(":warning: Suggestions can not be more then 1000 characters.")
+    #     embed = discord.Embed(color=colors.orange, title='New suggestion')
+    #     embed.set_author(icon_url=ctx.author.avatar.url, name=ctx.author.name + f' ({ctx.author.id} ')
+    #     embed.description = suggestion
+    #     msg = await channel.send(embed=embed)
+    #     await msg.add_reaction('👍')
+    #     await msg.add_reaction('👎')
+    #     await self.bot.database.execute("INSERT INTO suggestions(suggestion, user_id, msg_id) VALUES($1, $2, $3)", suggestion, ctx.author.id, msg.id)
 
-        embed = discord.Embed(color=colors.orange, title='New suggestion')
-        embed.set_author(icon_url=ctx.author.avatar.url, name=ctx.author.name + f' ({ctx.author.id} ')
-        embed.description = suggestion
-        msg = await channel.send(embed=embed)
-        await msg.add_reaction('👍')
-        await msg.add_reaction('👎')
-        await self.bot.database.execute("INSERT INTO suggestions(suggestion, user_id, msg_id) VALUES($1, $2, $3)", suggestion, ctx.author.id, msg.id)
-
-        e = discord.Embed(color=colors.orange)
-        e.description = f'Your suggestion has been sent to [our support server]({links.support})'
-        await ctx.send(embed=e)
+    #     e = discord.Embed(color=colors.orange)
+    #     e.description = f'Your suggestion has been sent to [our support server]({links.support})'
+    #     await ctx.send(embed=e)
 
     @commands.command()
     @commands.guild_only()
