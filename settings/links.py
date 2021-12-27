e07b38dedf5e09fc3e99f24544aec5bcac08f3e0
Joshua <ZoniqHedgehog@gmail.com>
2021-12-27 16:46:20 +0100

    update IDs and links

--- settings/links.py
+++ settings/links.py
@@ -1,17 +1,18 @@
 # External links
 
-support = 'https://discord.gg/gPZNGANWCE'
+support = 'https://discord.gg/w4JC9Yp'
+# INVITES OBSOLETE #
 invite = 'https://discord.com/api/oauth2/authorize?client_id=909560157116170250&permissions=412384349248&scope=bot%20applications.commands'  # Has slash commands and bot scope
 plaininvite = 'https://discord.com/api/oauth2/authorize?client_id=909560157116170250&permissions=412384349248&scope=bot'  # Only has bot scope
-github = 'https://github.com/BadWolf-Dev/BadWolf-bot'
+# INVITES OBSOLETE #
+github = 'https://github.com/ThePawKingdom/TPK-bot'
 
 # User IDs
 
 joshua = '843866750131109909'
-etile = '856910332290007071'
 
 # Channel IDs
-suggestions = 907617618180075580  # Channel to link suggestions to
-mainguild = 907286001188225084  # The bot's main support guild
-errors = 909541066011713577  # The channel to send errors to
-guildlog = 909557647664099409  # The channel to log guild join and leaves to
+suggestions = 907617618180075580  # Channel to link suggestions to - OBSOLETE.
+mainguild = 715969701771083817  # The bot's main support guild
+errors = 925049818601119795  # The channel to send errors to
+guildlog = 909557647664099409  # The channel to log guild join and leaves to - OBSOLETE.
